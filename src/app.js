cd78a792be8da860be48b174137b59cd1e6bd256
Demo User 33 <demouserXX@lab-cph.ibm.com>
2018-08-30 13:20:42 +0200

    added new endpoint

--- src/app.js
+++ src/app.js
@@ -16,5 +16,19 @@ app.get('/shipping/:itemID/:type', (request, response) => {
         })
 })
 
+app.get('/shipping', (request, response) => {
+    let ctrl = new ShippingController()
+
+    ctrl
+        .getItemShipping({id: request.query.itemId, type: request.query.type})
+        .then(amount => {
+            response.send({ product: request.query.itemId, priceUSD: amount})
+        })  
+
+        .catch(error => {
+            response.status(500).send({ error: error.message })
+        })
+})
+
 let PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log('ShippingService is listening on port ' + PORT))
